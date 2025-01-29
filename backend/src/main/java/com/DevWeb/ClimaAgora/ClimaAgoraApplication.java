111299a07621460f40afa5bd7ad5b2809261e206
gabriel <gabriel.lealsantos@hotmail.com>
2025-01-29 14:35:29 -0300

    Iniciando o projeto

---
+++ backend/src/main/java/com/DevWeb/ClimaAgora/ClimaAgoraApplication.java
@@ -0,0 +1,13 @@
+package com.DevWeb.ClimaAgora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaAgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaAgoraApplication.class, args);
+	}
+
+}
