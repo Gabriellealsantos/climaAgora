8d9e12011da97b17822ce0105eb1f19a0b661c83
Gabriellealsantos <gabriel.lealsantos@hotmail.com>
2025-06-17 13:47:15 -0300

    Adicionando variaveis de ambiente

--- frontend/src/utils/system.ts
+++ frontend/src/utils/system.ts
@@ -1,8 +1,8 @@
-export const BASE_URL = "http://localhost:8081";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const CLIENT_ID = "myclientid";
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
-export const CLIENT_SECRET = "myclientsecret";
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
 
-export const TOKEN_KEY = "com.clima.agora/Token";
+export const TOKEN_KEY = import.meta.env.VITE_TOKEN_KEY;
 
